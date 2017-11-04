01a97de67ba44bc4a624e424f3ce0d95a0726e1f
Aleksey939 <Aleksey93919@gmail.com>
2017-11-04 15:24:57 +0200

    Добавьте файлы проекта.

---
+++ Clock/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using System.Timers;
+using System.Windows;
+using System.Windows.Media;
+using Clock;
+
+namespace Clock
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+           
+          
+                InitializeComponent();
+           
+            
+            CompositionTarget.Rendering += rotate;
+
+        }
+
+        public void rotate(object sender, object args)
+        {
+            DateTime dt = DateTime.Now;
+
+            RotateTransform rotateTransform1 = new RotateTransform(DateTime.Now.Hour * 30 + DateTime.Now.Minute / 6 + DateTime.Now.Second / 120, 0.5, 0.5);
+            Hours.RenderTransform = rotateTransform1;
+
+            RotateTransform rotateTransform2 = new RotateTransform(DateTime.Now.Minute * 6 + DateTime.Now.Second / 10, 0.5, 0.5);
+            Minutes.RenderTransform = rotateTransform2;
+
+            RotateTransform rotateTransform3 = new RotateTransform(DateTime.Now.Second * 6, 0.5, 0.5);
+            Seconds.RenderTransform = rotateTransform3;
+
+        }
+       
+
+
+        }
+}
